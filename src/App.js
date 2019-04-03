ceb0d5a1fd5b588c7961f68c27b243b0b9e91026
Jin Park <jinpark1dev@gmail.com>
2019-04-02 19:00:55 -0700

    Added Inventory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Inventory from './Components/Inventory';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         This is App
+        <Inventory />
       </div>
     );
   }
---
+++ src/Components/Inventory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+  render() {
+    return (
+      <div className="Inventory">
+        This is Inventory
+      </div>
+    );
+  }
+}
+
+export default Inventory;

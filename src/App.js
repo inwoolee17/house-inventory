bb2b5dc3cda92086e83f4d2b817b35b0a442492d
Jin Park <jinpark1dev@gmail.com>
2019-04-02 18:58:40 -0700

    added text

--- src/App.js
+++ src/App.js
@@ -6,24 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-<<<<<<< HEAD
-            Edit <code>src/App.js</code> and save to JIN.
-=======
-            Edit <code>src/App.js</code> and save to start and start and start.
->>>>>>> 8193bb4be5178c57ee8310925a978afbd95cda7c
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        This is App
       </div>
     );
   }

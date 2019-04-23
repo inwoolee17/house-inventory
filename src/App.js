b3a182f664f8067e201f803026b29f231b89e770
Inwoo Lee <inoomong@gmail.com>
2019-04-23 13:12:34 -0700

    Added flex box

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
 import {Link} from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/">Home</Link>
-        <Link to="/inventory">Inventory</Link>
+        <div className="Mobile">
+          Mobile
+        </div>
+        <div className="Desktop">
+          <Link to="/">Home</Link>
+          <Link to="/inventory">Inventory</Link>
+        </div>
         {Routes}
       </div>
     );

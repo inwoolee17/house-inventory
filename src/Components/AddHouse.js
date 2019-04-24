5df01553912630f80d41de9fd52d72de2b29341b
Jin Park <jinpark1dev@gmail.com>
2019-04-23 21:50:12 -0700

    Added AddHouse Component

---
+++ src/Components/AddHouse.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './AddHouse.css';
+
+class AddHouse extends Component {
+  render() {
+    return (
+      <div className="AddHouse">
+        Add House
+      </div>
+    );
+  }
+}
+
+export default AddHouse;
